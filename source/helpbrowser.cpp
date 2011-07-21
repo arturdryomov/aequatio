42e47a38c7b38f70dd049c9e93a452093adb6350
Dmitry Savchenko <dmitry.a.sav@gmail.com>
2011-07-21 20:09:36 +0300

    HelpBrowser tuned. It now opens external links.

--- source/helpbrowser.cpp
+++ source/helpbrowser.cpp
@@ -4,9 +4,9 @@
 #include <QtHelp/QHelpEngine>
 
 HelpBrowser::HelpBrowser(QHelpEngine *helpEngine, QWidget *parent) :
-	QTextBrowser(parent)
+	QTextBrowser(parent), m_helpEngine(helpEngine)
 {
-	m_helpEngine = helpEngine;
+	this->setOpenExternalLinks(true);
 }
 
 QVariant HelpBrowser::loadResource(int type, const QUrl &name)
